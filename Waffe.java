8dbf3dcf3d6b0fa7558b66ff996ee8fb2a082f3b
AnnabellReichl <162974134+AnnabellReichl@users.noreply.github.com>
2026-09-17 13:45:14 +0200

    Update Waffe.java

--- Waffe.java
+++ Waffe.java
@@ -7,27 +7,12 @@
  */
 public class Waffe
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Waffe
-     */
-    public Waffe()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    //int bonus;
+    //String material;
+    //int magie;
+    // array mit materialien?
+    
+    
+            
+    
 }

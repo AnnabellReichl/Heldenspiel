1f747ca93726fdfc420f03d191762271e638315c
AnnabellReichl <162974134+AnnabellReichl@users.noreply.github.com>
2026-09-24 08:46:21 +0200

    Add files via upload

---
+++ Turnier.java
@@ -0,0 +1,31 @@
+public class Turnier {
+    
+    private Held[] h;
+    private Monster[] m;
+    
+    public Turnier(int pAnzahlHelden, int pAnzahlMonster) {
+        
+        h = new Held[pAnzahlHelden]; 
+        m = new Monster[pAnzahlMonster];
+        
+        for(int i = 0; i< h.length; i++) {
+            Wuerfel w = new Wuerfel(10);
+            w.werfen();
+            h[i] = new Held("liselotte_"+i, w.gibAugenzahl());
+        }
+        for(int i = 0; i< m.length; i++) {
+            m[i] = new Monster();
+        }
+        
+    }
+    public Held gibStaerkstenHeld() {
+        Held staerkster = h[0];
+        for(int i = 1; i<h.length; i++) {
+            if(h[i] != null && staerkster.getStaerke() < h[i].getStaerke()) {
+                staerkster = h[i];
+            }
+        }
+        return staerkster;
+    }
+    
+}
